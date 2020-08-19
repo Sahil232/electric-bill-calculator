6950d5d95c8f110d5cd2d6f813c92c509baaf992
Sahil <sahilpatel979@gmail.com>
2020-08-19 01:47:25 -0400

    Updates

---
+++ precache-manifest.b4e3261a0bd93a289f1afbc7c10890a4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cadfa2851f1ae4c74fc7c1848535e38f",
+    "url": "/electric-bill-calculator/index.html"
+  },
+  {
+    "revision": "a9530692c1a1507e8abc",
+    "url": "/electric-bill-calculator/static/css/main.7532985d.chunk.css"
+  },
+  {
+    "revision": "b9601c69c47bdcc4b70b",
+    "url": "/electric-bill-calculator/static/js/2.d70abd4c.chunk.js"
+  },
+  {
+    "revision": "1937a0fc6634269e5f47f8181e27fdc7",
+    "url": "/electric-bill-calculator/static/js/2.d70abd4c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a9530692c1a1507e8abc",
+    "url": "/electric-bill-calculator/static/js/main.e06ddab8.chunk.js"
+  },
+  {
+    "revision": "9f2f3c66267d39891fb5",
+    "url": "/electric-bill-calculator/static/js/runtime-main.fce47d87.js"
+  }
+]);
